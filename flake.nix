288223f2fe5515cb91b00357a63d49980548e8ee
Sighery <11218602+Sighery@users.noreply.github.com>
2026-09-11 00:36:25 +0200

    Remove flake-utils dependency
    I was using `flake-utils.lib.eachDefaultSystem` to create the same
    devShells for all available systems. Turns out nowadays it's not needed
    anymore. `nixpkgs.lib.genAttrs` and `nixpkgs.lib.systems.flakeExposed`
    can be composed together into pretty much the same thing, so I can avoid
    this whole dependency with a few lines of boilerplate.
    
    Relevant articles:
    https://ayats.org/blog/no-flake-utils#do-we-really-need-flake-utils
    https://nixcademy.com/posts/1000-instances-of-flake-utils/#the-alternative

--- flake.nix
+++ flake.nix
@@ -1,30 +1,27 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in {
-        devShells = {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              hugo
-              exiftool
-            ];
-          };
+  outputs = { self, nixpkgs }: let
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+        system: function nixpkgs.legacyPackages.${system}
+      );
+  in {
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          hugo
+          exiftool
+        ];
+      };
 
-          ci = pkgs.mkShellNoCC {
-            packages = with pkgs; [
-              hugo
-            ];
-          };
-        };
-      }
-    );
+      ci = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          hugo
+        ];
+      };
+    });
+  };
 }
